80a0519fc35a46d48fc6e7ceab8ac6b7aa2d4089
HackFun <hackfeng624@gmail.com>
2019-03-27 00:55:49 +0800

    项目初始化

---
+++ ad-eureka/src/main/java/com/hackfun/ad/EurekaApplication.java
@@ -0,0 +1,10 @@
+package com.hackfun.ad;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+}
